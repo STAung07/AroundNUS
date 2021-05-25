c921d9d6d6ad46af6254f7968a6efedbb567a4ca
STAung07 <swann1999@gmail.com>
2021-05-25 18:51:53 +0800

    Google Maps API added

--- around_nus/lib/map.dart
+++ around_nus/lib/map.dart
@@ -1,38 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+
 import './drawer.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'AroundNUS'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyMapPage extends StatefulWidget {
+  MyMapPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyMapPageState createState() => _MyMapPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyMapPageState extends State<MyMapPage> {
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
+
+  // change to USER Location later
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -40,12 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // This method is rerun every time setState is called
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
