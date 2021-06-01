039a762569f092957e97bbcd9dfb2da9acc8b992
STAung07 <swann1999@gmail.com>
2021-06-01 23:42:24 +0800

    Bus Timings does not support null safety commented out for now; fix tomorrow

--- around_nus/lib/app_screens/bustimings.dart
+++ around_nus/lib/app_screens/bustimings.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import '../common_widgets/drawer.dart';
-import 'package:nus_nextbus_api/nus_nextbus_api.dart';
 
-class BusTimings extends StatelessWidget {
+//import 'package:nus_nextbus_api/nus_nextbus_api.dart';
+//import 'package:nus_nextbus_api/src/models/BusStopsApi.dart';
+//import 'package:nus_nextbus_api/src/models/RouteApi.dart';
+//import 'package:nus_nextbus_api/src/models/ShuttlesApi.dart';
+class BusTimings extends StatefulWidget {
+  @override
+  _BusTimingsState createState() => _BusTimingsState();
+}
+
+class _BusTimingsState extends State<BusTimings> {
+//  var nextBusApi = NusNextBusApi();
+//  void _getMuseumBusTimings() async {
+//    ShuttlesApi shuttles = await nextBusApi.getShuttleTimings("MUSEUM");
+//    print(shuttles.shuttles);
+//  }
+
+  //@override
+  //void initState() {
+  //  super.initState();
+  //  //_getMuseumBusTimings();
+  //}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
